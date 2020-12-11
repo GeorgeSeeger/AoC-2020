02b7eca483b83ad3a87578a72bba224b8a5ae05f
George Seeger <george@polylytics.com>
2020-12-11 16:29:12 +0000

    day 6 part 2

--- day6.js
+++ day6.js
@@ -16,3 +16,17 @@ const part1 = _.chain(lines).map((group) => {
 .value();
 
 console.log("part1: " + part1);
+
+const part2 = _.chain(lines).map((group) => {
+    var answers = group.split("\n")
+    return _.chain(answers)
+    .join("").split("")
+    .groupBy(a => a)
+    .filter(numAnswers => numAnswers.length === answers.length)
+    .size()
+    .value();
+})
+.sum()
+.value();
+
+console.log("part2: " + part2);

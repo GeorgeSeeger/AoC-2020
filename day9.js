cbb632372d84208b3c377d5cbdddcbf0152aae7e
georgeseeger <georgeseeger@hotmail.com>
2020-12-12 12:27:28 +0000

    day 9 pt 2 ez4me

--- day9.js
+++ day9.js
@@ -8,8 +8,7 @@ const isValid = (val, index) => {
   return _.chain(lines).slice(index).take(preamble)
   .some((i, _ind, collection) =>
     _.some(collection, j => j + i === val && j !== i)
-  )
-  .value();
+  ).value();
 }
 
 const part1 = _.chain(lines).slice(preamble)
@@ -17,3 +16,25 @@ const part1 = _.chain(lines).slice(preamble)
 .value();
 
 console.log("part1: " + part1);
+
+var sumLength = 0;
+const sumsToPart1  = _.chain(lines)
+  .findIndex((val, ind, collection) => {
+    let sum = val;
+    while (sum <= part1) {
+       sum += collection[ind + (++sumLength)]
+       if (sum === part1) return true;
+    }
+    sumLength = 0;
+    return false;
+  })
+  .value();
+
+const part2 = _.chain(lines)
+.slice(sumsToPart1, sumsToPart1 + sumLength + 1)
+.sortBy(i => i)
+.filter((val, ind, sums) => ind === 0 || ind === sums.length - 1)
+.sum()
+.value();
+
+console.log("part2: " + part2);

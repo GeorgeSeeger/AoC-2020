03a6ef343a214be794e50323eaec76034f80a6d6
George Seeger <georgeseeger@hotmail.com>
2020-12-13 12:33:16 +0000

    day 12 part 2

--- day12.js
+++ day12.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const lines = fs.readFileSync("./day12.input", 'utf8').trim().split("\n");
 
-function makeInstr() {
+function part1Instructions() {
     let shipDir = "E";
 
     const turn = (rOrL, pos, angle) => {
@@ -30,9 +30,8 @@ function makeInstr() {
 
     return instrs;
 }
-
-const part1 = (function() {
-    let pos = { x: 0, y: 0 }
+function solve(makeInstr) {
+    let pos = { x: 0, y: 0};
     let instrs = makeInstr();
     lines.forEach(line => {
         let instr = line[0];
@@ -40,6 +39,38 @@ const part1 = (function() {
         instrs[instr](pos, arg);
     })
     return Math.abs(pos.x) + Math.abs(pos.y);
-})();
+}
+const part1 = solve(part1Instructions);
 
 console.log("part1: " + part1);
+
+function part2Instructions() {
+    let wp = { x: 10, y: 1}
+
+    const instrs = {
+        N: (pos, dist) => { wp.y += dist },
+        S: (pos, dist) => { wp.y -= dist },
+        E: (pos, dist) => { wp.x += dist },
+        W: (pos, dist) => { wp.x -= dist },
+        F: (pos, dist) => { for (let i = 0; i < dist; i++) { pos.x += wp.x; pos.y += wp.y }},
+        R: (pos, angle) => {
+            for (let i = 0; i < angle; i += 90) {
+                let x = wp.x;
+                wp.x = wp.y;
+                wp.y = -x;
+            }
+        },
+        L: (pos, angle) => {
+            for (let i = 0; i < angle; i += 90) {
+                let x = wp.x;
+                wp.x = -wp.y;
+                wp.y = x;
+            }
+        }
+    }
+    return instrs;
+}
+
+const part2 = solve(part2Instructions);
+
+console.log("part2: " + part2);

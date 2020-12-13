2d1730ba2dc87b3f1faa075028a62c5ff769159e
George Seeger <georgeseeger@hotmail.com>
2020-12-13 10:20:38 +0000

    day 10 part 2 omfg nearly got filtered

--- day10.js
+++ day10.js
@@ -15,3 +15,29 @@ const part1 = _.chain(allChargers).groupBy((val) => {
 .value();
 
 console.log("part1: " + part1);
+
+allChargers.push(_.last(allChargers) + 3);
+
+const part2 = (function() {
+    const memo = {};
+    const inner = function(chargers) {
+        var key = chargers.join("-");
+        if (memo[key]) { return memo[key]; }
+        if (chargers.length === 2) {
+            return 1;
+        }
+
+        var val = chargers[0];
+        var combos = 0;
+        for (let i = 1; i <= Math.min(3, chargers.length - 1); i++) {
+            if (chargers[i] - val <= 3) {
+                combos += inner(chargers.slice(i))
+            }
+        }
+        memo[key] = combos;
+        return combos;
+    }
+    return inner;
+})()(allChargers)
+
+console.log("part2: " + part2 );

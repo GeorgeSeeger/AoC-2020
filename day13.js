5d473787ab4e75246336055e6b6144727858745d
georgeseeger <georgeseeger@hotmail.com>
2020-12-13 20:07:39 +0000

    Day 13 Part 2 and we're all caught up ;)

--- day13.js
+++ day13.js
@@ -4,9 +4,10 @@ const fs = require("fs");
 const lines = fs.readFileSync("./day13.input", 'utf8').trim().split("\n");
 
 const earliest = parseInt(lines[0])
-const part1 = _.chain(lines[1].split(","))
-.filter(i => i !== 'x')
-.map(i => parseInt(i))
+const busIds = lines[1].split(",").map(i => parseInt(i));
+
+const part1 = _.chain(busIds)
+.reject(isNaN)
 .map(id => { return {id: id, time:( id - (earliest % id))} })
 .sortBy(b => b.time)
 .map(b => b.id * b.time)
@@ -14,3 +15,27 @@ const part1 = _.chain(lines[1].split(","))
 .value();
 
 console.log("part1: " + part1);
+
+const part2 = _.chain(busIds)
+.map((i, ind) => isNaN(i) ? false : { id: BigInt(i), offset: BigInt(ind) })
+.compact()
+.reduce((acc, bus) => {
+  if (bus.offset === 0n) {
+    acc.multiple = bus.id;
+    acc.start = bus.id;
+    return acc;
+  }
+
+  let i = BigInt(0);
+  for (i = acc.start;
+      bus.id - (i % bus.id) !== (bus.offset % bus.id);
+      i += acc.multiple) {}
+
+  acc.start = i;
+  acc.multiple *= bus.id;
+  return acc;
+}, {})
+.value()
+.start;
+
+console.log("part2: " + part2);

db94ee1136d2ca0fc4d8fc2bcaee9324c37264c9
George Seeger <georgeseeger@hotmail.com>
2020-12-18 11:57:52 +0000

    day 18 part 2 nbd

--- day18.js
+++ day18.js
@@ -8,22 +8,21 @@ const funcs = {
     "*": (a, b) => a * b
 }
 
-function calcPart1(input) {
-    if (input.length === 1) return input[0];
-    if (input.length < 1) return 0;
-
+function calcLtoR(input) {
     let a = input.shift();
     if (a === "(") {
-        a = calcPart1(takeBrackets());
+        a = calcLtoR(takeBrackets());
     }
     let op = input.shift();
     let b = input.shift();
     if (b === "(") {
-        b = calcPart1(takeBrackets());
+        b = calcLtoR(takeBrackets());
     }
 
     input.unshift(funcs[op](+a, +b))
-    return calcPart1(input);
+    return input.length === 1 
+            ? input[0]
+            : calcLtoR(input);
 
     function takeBrackets() {
         let bCount = 1;
@@ -40,8 +39,52 @@ function calcPart1(input) {
 
 const part1 = _.chain(input)
                 .map(str => str.replace(/\s/g, "").split(""))
-                .map(calcPart1)
+                .map(calcLtoR)
                 .sum()
                 .value()
 
 console.log("part1: " + part1);
+
+function calcPart2(input) {
+    for (let braIndex = input.indexOf("(");
+     braIndex >= 0; 
+     braIndex = input.indexOf("(")) {
+
+        let subExpr = takeBrackets(input.slice(braIndex + 1))
+        input = input.slice(0, braIndex)
+        .concat(calcPart2(subExpr.slice(0))) // evil side effects!
+        .concat(input.slice(braIndex + subExpr.length + 2))
+    }
+
+    for (addIndex = input.indexOf("+");
+    addIndex >= 0;
+    addIndex = input.indexOf("+")) {
+
+        let addResult = +input[addIndex - 1] + (+input[addIndex + 1])
+        input = input.slice(0, addIndex - 1).concat(addResult).concat(input.slice(addIndex + 2));
+    }
+
+    if (input.length === 1) return input[0];
+
+    return calcLtoR(input);
+    
+    function takeBrackets(input) {
+        let bCount = 1;
+        let inner = []
+        while (bCount > 0) {
+            let c = input.shift();
+            if (c === "(") bCount++;
+            if (c === ")") bCount--;
+            if (bCount > 0) inner.push(c);
+        }
+        return inner;
+    }
+}
+
+const part2 = _.chain(input)
+                .map(str => str.replace(/\s/g, "").split(""))
+                .map(calcPart2)
+                .sum()
+                .value()
+
+console.log("part2: " + part2);

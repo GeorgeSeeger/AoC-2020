946597c65ce19e818c9891c0f4a4566c36c43b3e
George Seeger <georgeseeger@hotmail.com>
2020-12-20 23:43:37 +0000

    day 20 part 2 what the HELL was that

--- day20.js
+++ day20.js
@@ -1,28 +1,29 @@
 const _ = require("lodash");
 const fs = require("fs");
-const { partialRight } = require("lodash");
 
 const input = fs.readFileSync("./day20.input", "utf-8").trim().split("\n\n");
 
 function rotate(array, n) {
     let len = array.length;
-    return array.map((line, j) => {
+    const rot = [];
+    for (let j = 0; j < array.length; j++) {
+        let line = array[j];
         let lineLen = line.length;
-        return line.map((v, i) => {
+        for (let i = 0; i < line.length; i++) {
+            if (( n === 1 || n === 3) && !rot[i] 
+               || n === 2 && !rot[j]) rot.push([]) 
             if (n === 1) {
-                return array[i][len - j - 1]
+                rot[i][j] = array[len - j - 1][i];
             }
             if (n === 2) {
-                return array[len - j - 1][len - i - 1]
+                rot[j][i] = array[len - j - 1][lineLen - i - 1]
             }
             if (n === 3) {
-                return array[len - i - 1][j]
+                rot[i][j] = array[j][lineLen - i - 1];
             }
-            if (n === 4) {
-                return v
-            }
-        })
-    })
+        }
+    }
+    return rot;
 }
 
 function flip(array, isVert, isHorz) {
@@ -84,25 +85,96 @@ class Tile {
     }
 }
 
-const tiles = input.map(Tile.parse)
 
-let allTransforms = ti => _.chain([ti, ti.flipH()]).map(ti => [ti, ti.rotate90(), ti.rotate180(), ti.rotate270()]).flatten().value();
+const image = (function() {
+    const tiles = input.map(Tile.parse)
 
-const part1 = _.chain(tiles)
-.filter((tile) => {
-    return _.chain(tiles)
-    .filter(t => t !== tile)
-    .map(allTransforms)
-    .flatten()
-    .filter(t => t.top      === tile.bottom
-                || t.bottom === tile.top
-                || t.left   === tile.right
-                || t.right  === tile.left 
-                )
-    .size()
-    .value() === 2
-})
-.reduce((acc, t) => acc * t.id, 1)
-.value();
+    let allTransforms = ti => _.chain([ti, ti.flipH()]).map(ti => [ti, ti.rotate90(), ti.rotate180(), ti.rotate270()]).flatten().value();
+
+    let n = Math.sqrt(_.size(tiles));
+    let image = Array.from({length: n}, () => Array.from({length: n}, () => null));
+
+    let tileTransforms = _.flatten(tiles.map(allTransforms));
+    
+    let topCorner = _.find(tiles, tile => 
+        _.chain(tileTransforms)
+        .reject(t => t.id == tile.id)
+        .every(t => t.bottom !== tile.top
+            && t.right !== tile.left)
+        .value());
+
+    let removeTileFromPool = tile => _.remove(tileTransforms, t => t.id === tile.id);
+    removeTileFromPool(topCorner);
+
+    image[0][0] = topCorner;
+
+    for (let j in image) {
+        for (let i in image[j]) {
+            if (j === '0' && i === '0') continue;
+            let predicate = i !== '0' 
+                    ? t => t.left === image[j][i - 1].right
+                    : t => t.top === image[j - 1][0].bottom;
+            let next = _.find(tileTransforms, predicate);
+            removeTileFromPool(next);
+            image[j][i] = next;
+        }
+    }
+
+    return image;
+})()
+
+const part1 = [image[0][0], image[0][image.length - 1], image[image.length - 1][0], image[image.length - 1][image.length - 1]]
+            .reduce((acc, t) => acc * t.id, 1);
 
 console.log("part1: " + part1)
+
+const imageData = _.chain(image)
+                    .map(row => row.map(t => t.data
+                                              .slice(1, t.data.length - 1)
+                                              .map(line => line.slice(1, line.length - 1))))
+                    .map(_.flatten)
+                    .map(coll => coll.reduce((acc, line, i) => {
+                        let newIndex = i % line.length;
+                        if (!acc[newIndex]) acc[newIndex] = [];
+                        acc[newIndex].push(line)
+                        return acc;
+                     }, []))
+                     .map(coll => coll.map(_.flatten))
+                     .flatten()
+                    .value()
+
+const seaMonster = [
+"                  O ",
+"O    OO    OO    OOO",
+" O  O  O  O  O  O   "].map(l => 
+    l.split("").map(c => c === "O" 
+    ? { isMonster: c => c === "#", mark: c => "O" }
+    : { isMonster: c => true, mark: c => c })
+);
+
+const allSeaMonsters = _.chain([seaMonster, flip(seaMonster, true, false)])
+.map(sm => [ sm, rotate(sm, 1), rotate(sm, 2), rotate(sm, 3)])
+.flatten()
+.value();
+
+for (let sm of allSeaMonsters) {
+    for (let j = 0; j < imageData.length - sm.length; j++) {
+        for (let i = 0; i < imageData[0].length - sm[0].length; i++) {
+            
+            let isSeaMonster = sm.every((smLine, m) => smLine.every((smCell, n) => {
+                return smCell.isMonster(imageData[j + m][i + n]);
+            }));
+
+            if (isSeaMonster) {
+                sm.forEach((smLine, m) => smLine.forEach((smCell, n) => {
+                    imageData[j + m][i + n] = smCell.mark(imageData[j + m][i + n]);
+                }));
+            }
+        }
+
+    }
+}
+
+const part2 = _.chain(imageData).flatten().filter(c => c === "#").size().value();
+                  
+console.log("part2: " + part2);

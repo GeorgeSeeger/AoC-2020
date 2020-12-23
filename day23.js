292d778a89513aaad480066fed5a08d6e1053bbf
George Seeger <georgeseeger@hotmail.com>
2020-12-23 23:52:48 +0000

    day23 part 2 fkn ell

--- day23.js
+++ day23.js
@@ -1,29 +1,60 @@
 const _ = require("lodash");
 
-const inputPart1 = '624397158'.split("").map(i => +i);
+const input = '624397158'.split("").map(i => +i);
 
-const moves = 100;
+let moves = 100;
 
-var cups = inputPart1;
-var currPos = 0;
-for (let i = 0; i < moves; i++) {
-    let current = cups[currPos];
-    currPos = cups.indexOf(current);
-    let pickUp = cups.slice(currPos + 1, (currPos + 4) > cups.length ? cups.length : currPos + 4).concat(cups.slice(0, currPos + 4 > cups.length ? currPos + 4 - cups.length : 0));
-    cups = _.difference(cups, pickUp);
-    let destination = (() => {
-        let dest = current - 1;
-        let min = Math.min(...cups);
-        let max = Math.max(...cups);
-        while (!cups.includes(dest)) {
-            dest = cups.includes(dest - 1) || dest - 1 > min ? dest - 1 : max;
-        }
-        return dest;
-    })();
-    let destPos = cups.indexOf(destination);
-    cups.splice((destPos + 1), 0, ...pickUp);
-    currPos = (cups.indexOf(current) + 1) % cups.length;
+function playGame(cups, moves, topUpTo) {
+    let current = _.first(cups);
+
+    let limit = topUpTo || _.last(cups);
+    let max = topUpTo || Math.max(...cups);
+    let pointerLookup = input.reduce((acc, v, i, c) => {
+        acc[v] = i < c.length - 1 ? c[i+1] : c.length + 1
+        return acc;
+    }, {});
+    
+    pointerLookup[limit] = _.first(cups); // loop it back on itself
+
+    let find = n => {
+        if (_.has(pointerLookup, n)) return pointerLookup[n];
+        
+        return (pointerLookup[n] = n + 1);
+    }
+
+    for (let i = 0; i < moves; i++) {
+        let t = current;
+        let pickUp = _.range(3).map(() => {
+            let v = find(t);
+            t = v;
+            return v;
+        });
+
+        let destination = _.chain(_.range(current - 1, current - 4, -1))
+        .concat(_.range(max, max - 3, -1))
+        .reject(i => pickUp.includes(i))
+        .reject(i => i < 1)
+        .first().value();
+
+        pointerLookup[current] = find(_.last(pickUp));
+        pointerLookup[_.last(pickUp)] = find(destination);
+        pointerLookup[destination] = _.first(pickUp);
+        current = find(current);
+    }
+
+    return pointerLookup;
 }
 
-const part1 = cups.slice(cups.indexOf(1) + 1).concat(cups.slice(0, cups.indexOf(1))).join("")
+let lk = playGame(input, moves);
+const part1 = input.reduce((acc, v, i, c) => {
+    const next = i === 0 ? lk[1] : lk[acc[i - 1]];
+    if (next === 1) return acc;
+    acc.push(next)
+    return acc;
+}, []).join("");
+
 console.log("part1: " + part1);
+
+lk = playGame(input, 1e7, 1e6);
+const part2 = lk[1] * lk[lk[1]]
+console.log("part2: " + part2);
